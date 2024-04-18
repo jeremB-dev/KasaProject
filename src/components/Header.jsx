be7a25a5e06de74d0a497e3413df823518a0489f
Jerem b <jerem.brunel.33@gmail.com>
2024-04-18 11:51:09 +0200

    ajout sass et essai construction application

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="" alt="logo" />
+             <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
