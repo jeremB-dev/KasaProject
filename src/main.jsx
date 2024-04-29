33325766c495035366b5757767746272ec7b5639
Jerem b <jerem.brunel.33@gmail.com>
2024-04-29 14:47:18 +0200

    probleme scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './assets/sass/app.scss'
+import './assets/sass/header.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
