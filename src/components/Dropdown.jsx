85191392f601fad47f0eb4a65feed47358984e90
Jerem b <jerem.brunel.33@gmail.com>
2024-05-14 13:59:33 +0200

    ajout Dropdown sur la page About

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import ArrowDown from "../assets/img-projet/ArrowDown.png";
+import PropTypes from "prop-types";
+
+const Dropdown = ({ title, children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const handleToggle = () => {
+        setIsVisible(!isVisible);
+    }
+
+    return (
+        <div className={`Dropdown ${isVisible ? "visible" : ""}`}>
+            <div className="dropdownTitle" onClick={handleToggle}>
+                <h3>{title}</h3>
+                <img src={ArrowDown} alt="flèche" className={`arrow ${isVisible ? "down" : ""}`}/>
+            </div>
+            <div className={`paragraph ${isVisible ? "down" : ""}`}>
+                {children}
+            </div>    
+        </div>
+    );
+};
+
+Dropdown.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+export default Dropdown;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,16 @@
 import BannerAbout from "../components/BannerAbout";
-
+import Dropdown from "../components/Dropdown";
+import CollapseJson from "../assets/data/collapse.json";
 
 const About = () => {
     return (
         <div className="about">
-               <BannerAbout /> 
+               <BannerAbout />
+               {CollapseJson.map((collapse, index) => (
+                <Dropdown key={index} index={index} title={collapse.title}>
+                    {collapse.text}
+                </Dropdown>
+               ))} 
         </div>
     );
 };
