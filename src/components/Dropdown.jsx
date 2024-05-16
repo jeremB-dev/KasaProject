4395a8c1b7f5173395465f463884c7ca5ca31977
Jerem b <jerem.brunel.33@gmail.com>
2024-05-16 09:04:36 +0200

    Dropdown/scss terminer

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
+import { useState } from "react";    // ajoute un état local
 import ArrowDown from "../assets/img-projet/ArrowDown.png";
 import PropTypes from "prop-types";
 
 const Dropdown = ({ title, children }) => {
     const [isVisible, setIsVisible] = useState(false);
-    const handleToggle = () => {
+    const pullDown = () => {
         setIsVisible(!isVisible);
     }
 
     return (
-        <div className={`Dropdown ${isVisible ? "visible" : ""}`}>
-            <div className="dropdownTitle" onClick={handleToggle}>
+        <div className={`dropdown ${isVisible ? "visible" : ""}`}>
+            <div className="dropdownTitle" >
                 <h3>{title}</h3>
-                <img src={ArrowDown} alt="flèche" className={`arrow ${isVisible ? "down" : ""}`}/>
+                <img src={ArrowDown} alt="flèche" className={`arrow ${isVisible ? "down" : ""}`} onClick={pullDown} />
             </div>
-            <div className={`paragraph ${isVisible ? "down" : ""}`}>
+            <div className={`paragraph ${isVisible ? "animation" : ""}`}>
                 {children}
             </div>    
-            
         </div>
     );
 };
