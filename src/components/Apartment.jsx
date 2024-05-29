6e98e9f8beee4a5120c920dcae876506b114d0b5
Jerem b <jerem.brunel.33@gmail.com>
2024-05-29 08:57:53 +0200

    suite responsive

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -7,46 +7,46 @@ import Caroussel from "./Caroussel";
 import Tags from "./Tags";
 
 const Apartment = () => {
-    // Récupère les paramètres de l'URL.
-    const { id } = useParams(); 
-    // Permet de naviguer entre les pages.
-    const navigate = useNavigate();
-    // Récupère les données du logement correspondant à l'ID.
-    const apartment = logements.find((item) => item.id === id); //null ou undefined
+  // Récupère les paramètres de l'URL.
+  const { id } = useParams();
+  // Permet de naviguer entre les pages.
+  const navigate = useNavigate();
+  // Récupère les données du logement correspondant à l'ID.
+  const apartment = logements.find((item) => item.id === id); //null ou undefined
 
-    // Redirige vers la page d'erreur si l'ID n'existe pas.
-    useEffect(() => {
-        if (!apartment) {
-            navigate('/Error');
-        }
-    }, [apartment, navigate, id]);
-    
+  // Redirige vers la page d'erreur si l'ID n'existe pas.
+  useEffect(() => {
     if (!apartment) {
-        return null 
+      navigate('/Error');
     }
+  }, [apartment, navigate, id]);
 
- return (
+  if (!apartment) {
+    return null
+  }
+
+  return (
     <>
       <div className="appt">
-        <Caroussel slides={apartment.pictures}/>
+        <Caroussel slides={apartment.pictures} />
         <div className="appt-container">
           <div className="appt-container-up">
             <div className="appt-container-up-left">
               <h2 className="appt-title">{apartment.title}</h2>
               <h3 className="appt-loc">{apartment.location}</h3>
             </div>
-              <div className="container-tags">
-                {apartment.tags.map((tag, index) => (
-                  <Tags key={index} text={tag} />
-                ))}
-              </div>
+            <div className="container-tags">
+              {apartment.tags.map((tag, index) => (
+                <Tags key={index} text={tag} />
+              ))}
+            </div>
             <div className="container-hostRating">
               <div className="host">
                 <h3 className="host-name">{apartment.host.name}</h3>
                 <img className="host-img" src={apartment.host.picture} alt={apartment.title} />
               </div>
               <div className="rating">
-                <Rating rating = {parseInt(apartment.rating,10)}/>
+                <Rating rating={parseInt(apartment.rating, 10)} />
               </div>
             </div>
           </div>
