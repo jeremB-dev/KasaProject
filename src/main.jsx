b7ca93e221e64527fd980f07d6bd078e970e7ce4
brunel jeremy <jerem.brunel.33@gmail.com>
2025-05-19 17:21:04 +0200

    modif pour deploiement sur github pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@
 import React from 'react'; // est importé pour utiliser les fonctionnalités de React.
 import ReactDOM from 'react-dom/client'; //est importé pour gérer le rendu des composants dans le DOM.
 import App from './App.jsx'; // App est importé pour afficher le composant App dans le DOM.
+import { HashRouter } from 'react-router-dom';
 
 // Crée un root pour le rendu des composants dans le DOM grace a l'id de la page html.
 ReactDOM.createRoot(document.getElementById('root')).render( 
   <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
     <App />
   </React.StrictMode>,
 )
